702692be4bc4af3a887290998a636cc7fe10b21e
oscar blum <oscar.blum@gmail.com>
2022-11-30 12:58:59 -0500

    test pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -13,10 +13,10 @@ WickedPdf.config = {
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
-  exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf'),
+  # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf'),
 
   # Needed for wkhtmltopdf 0.12.6+ to use many wicked_pdf asset helpers
-  enable_local_file_access: true,
+  # enable_local_file_access: true,
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
