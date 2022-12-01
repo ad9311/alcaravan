629c3770fab46a9220259d6b3f698598029d3a14
oscar blum <oscar.blum@gmail.com>
2022-12-01 01:38:49 -0500

    fix error new session

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,9 @@
 module Users
   class SessionsController < Devise::SessionsController
-    skip_before_action :verify_authenticity_token, only: %i[new create]
+    protected
+
+      def after_sign_in_path_for(resource)
+        root_path
+      end
   end
 end
